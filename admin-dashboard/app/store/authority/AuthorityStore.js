0b732ab9e5311514134a7822eb7c38b0a6e4e04b
quokka <598959863@qq.com>
2017-10-08 22:33:57 +0800

    [更新]解决用户权限管理板块的前端页面

--- admin-dashboard/app/store/authority/AuthorityStore.js
+++ admin-dashboard/app/store/authority/AuthorityStore.js
@@ -21,22 +21,22 @@ Ext.define('Admin.store.authority.AuthorityStore', {
 	remoteSort: true,//全局排序
     sorters: {
         direction: 'DESC',
-        property: 'roleId'
+        property: 'userId'
     }
 });
 
 //假数据测试
-// Ext.define('Admin.store.role.RoleStore', {
+// Ext.define('Admin.store.authority.AuthorityStore', {
 //     extend: 'Ext.data.Store',
-//     alias: 'store.roleStore',       //1.Store取别名（reference）
-//     model: 'Admin.model.role.RoleModel',//2.设置model的全路径
+//     alias: 'store.authorityStore',       //1.Store取别名（reference）
+//     model: 'Admin.model.authority.AuthorityModel',//2.设置model的全路径
 //   proxy: {
 //     //type: 'ajax',
 //     type: 'memory',
 //     //url: 'role/findPage.json',  //后台OrderController中的接口url地址
 //     data:{
 //     'items':[
-//     { 'roleId': 1, 'roleName': 'Lisa',  "roleLevel":"1",  "modulesText":"555-111-1224"  }
+//     { 'userId': 1 ,  'roleId' : 2 , 'userName': 'zjk', 'roleName': 'r1', "modulesText":"555-111-1224"  }
 //     ]},
 //     reader: {
 //       type:'json', 
@@ -48,9 +48,9 @@ Ext.define('Admin.store.authority.AuthorityStore', {
 //   },
 //   pageSize: 25,
 //   autoLoad: true,
-//   remoteSort: true,//全局排序
+// 	remoteSort: true,//全局排序
 //     sorters: {
 //         direction: 'DESC',
-//         property: 'id'
+//         property: 'userId'
 //     }
 // });
